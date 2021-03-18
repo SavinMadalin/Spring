ed1c533a1f9a52d03da323b8a7cdcc5b80828f56
Madalin Savin <madalin3_ba@yahoo.com>
2021-03-18 09:39:12 +0200

    import sources to repository

---
+++ Spring/src/main/java/com/example/Spring/CarAssembler.java
@@ -0,0 +1,21 @@
+package com.example.Spring;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CarAssembler implements RepresentationModelAssembler<Car, EntityModel<Car>> {
+
+	@Override
+	public EntityModel<Car> toModel(Car car) {
+			return EntityModel.of(car,
+					linkTo(methodOn(CarController.class).findCarsByPersonId(car.getPerson().getId())).withRel("cars by person"),
+					linkTo(methodOn(CarController.class).findCarsByBrand(car.getBrand())).withRel("cars by brand"),
+					linkTo(methodOn(CarController.class).findAllCars()).withRel("all cars"),
+					linkTo(methodOn(CarController.class).getCar(car.getId())).withSelfRel()
+					);
+	}
+
+}
